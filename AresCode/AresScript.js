94a21e98a163ebd49958207e87484c8caeef6200
SantiagoRodriguez318 <Santiagorodriguez1402@outlook.com>
2025-10-20 19:05:05 -0300

    Front - 1er Commit - Cambio en estilos y colores

---
+++ AresCode/AresScript.js
@@ -0,0 +1,8 @@
+function toggleInfoWindow(){
+    const InfoWindow = document.getElementById("InfoWindow");
+    InfoWindow.style.display = InfoWindow.style.display ==="block" ? "none" : "block";
+}
+function toggleSocialWindow(){
+    const SocialWindow = document.getElementById("SocialWindow");
+    SocialWindow.style.display = SocialWindow.style.display ==="block" ? "none" : "block";
+}
